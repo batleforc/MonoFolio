3cc0f06feb88d2f058d2b365a51d2a1fdbeaac84
Max batleforc <maxleriche.60@gmail.com>
2024-08-04 23:56:41 +0200

    feat: mise en place home profil

--- apps/back/src/homeprofil/home.rs
+++ apps/back/src/homeprofil/home.rs
@@ -1,15 +1,20 @@
+use std::{fs::File, io::Read};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct HomeContent {
     pub name: String,
     pub presentation: String,
     pub cover_title: Vec<String>,
     pub cv_url: String,
     pub url: Vec<HomeUrl>,
+    pub history: Vec<HomeHistory>,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct HomeUrl {
     pub url: String,
     pub name: String,
@@ -18,6 +23,7 @@ pub struct HomeUrl {
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct HomeHistory {
     pub title: String,
     pub lieux: String,
@@ -29,7 +35,104 @@ pub struct HomeHistory {
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct HomeHistoryUrl {
     pub url: String,
     pub name: String,
+}
+
+#[derive(Serialize, Debug, PartialEq, Clone)]
+pub enum HomeLoadingError {
+    FileNotFound(String),
+    FileNotProperYaml(String),
+}
+
+pub fn load_home_content(base_path: &str) -> Result<HomeContent, HomeLoadingError> {
+    let path = format!("{}/home.yaml", base_path);
+    let mut file = match File::open(&path) {
+        Ok(file) => file,
+        Err(err) => return Err(HomeLoadingError::FileNotFound(err.to_string())),
+    };
+    let mut buffer = String::new();
+    let _ = file.read_to_string(&mut buffer);
+    let content: HomeContent = match serde_yaml::from_str(&buffer) {
+        Ok(content) => content,
+        Err(err) => return Err(HomeLoadingError::FileNotProperYaml(err.to_string())),
+    };
+    Ok(content)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::*;
+
+    #[test]
+    fn test_load_home_content() {
+        let mut path_buf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path_buf.push("../../test_dataset/content/");
+        let content = load_home_content(path_buf.to_str().unwrap());
+        println!("{:?}", content);
+        assert!(content.is_ok());
+        let content = content.unwrap();
+        assert_eq!(content.name, "max truc");
+        assert_eq!(content.presentation, "Aloha !\n");
+        assert_eq!(
+            content.cover_title,
+            vec!["dev".to_string(), "ops".to_string()]
+        );
+    }
+
+    #[test]
+    fn test_load_home_content_file_not_found() {
+        let mut path_buf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path_buf.push("../../test_dataset/empty/");
+        let content = load_home_content(path_buf.to_str().unwrap());
+        assert!(content.is_err());
+        let content = content.unwrap_err();
+        assert_eq!(
+            content,
+            HomeLoadingError::FileNotFound("No such file or directory (os error 2)".to_string())
+        );
+    }
+
+    #[test]
+    fn test_load_home_content_file_not_proper_yaml() {
+        let mut path_buf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path_buf.push("../../test_dataset/bad_content/");
+        let content = load_home_content(path_buf.to_str().unwrap());
+        assert!(content.is_err());
+    }
+
+    #[test]
+    fn test_home_serialize_deserialize() {
+        let home = HomeContent {
+            name: "max truc".to_string(),
+            presentation: "Aloha !\n".to_string(),
+            cover_title: vec!["dev".to_string(), "ops".to_string()],
+            cv_url: "https://cv.com".to_string(),
+            url: vec![HomeUrl {
+                url: "https://cv.com".to_string(),
+                name: "cv".to_string(),
+                primaire: true,
+                img_url: "https://cv.com".to_string(),
+            }],
+            history: vec![HomeHistory {
+                title: "title".to_string(),
+                lieux: "lieux".to_string(),
+                date: "date".to_string(),
+                weight: 1,
+                img_url: "https://cv.com".to_string(),
+                description: "description".to_string(),
+                url: Some(vec![HomeHistoryUrl {
+                    url: "https://cv.com".to_string(),
+                    name: "cv".to_string(),
+                }]),
+            }],
+        };
+        let serialized = serde_yaml::to_string(&home).unwrap();
+        let deserialized: HomeContent = serde_yaml::from_str(&serialized).unwrap();
+        assert_eq!(home, deserialized);
+    }
 }
