5b14f1be2fc5ca2f18389813414459168fb8da2b
Max batleforc <maxleriche.60@gmail.com>
2024-08-11 02:39:44 +0200

    feat: setup env variable for vite config

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,48 +1,58 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/front',
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  process.env.VITE_API_URL =
+    process.env.VITE_API_URL || process.env.ENV === 'production'
+      ? ''
+      : 'http://localhost:5437/api';
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
+  return defineConfig({
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/front',
 
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [vue(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  build: {
-    outDir: '../../dist/apps/front',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    server: {
+      port: 4200,
+      host: 'localhost',
     },
-  },
 
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/apps/front',
-      provider: 'v8',
+    preview: {
+      port: 4300,
+      host: 'localhost',
     },
-  },
-});
+
+    plugins: [vue(), nxViteTsPaths()],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+
+    build: {
+      outDir: '../../dist/apps/front',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+
+    test: {
+      watch: false,
+      globals: true,
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+      reporters: ['default'],
+      coverage: {
+        reportsDirectory: '../../coverage/apps/front',
+        provider: 'v8',
+      },
+    },
+  });
+};
